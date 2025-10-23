8ccb17aba09b7123d6f4cca3989b33db670dae9a
KenjiOM <kenjimorimoto08@gmail.com>
2025-10-22 21:30:02 -0300

    correções

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_argon2 import Argon2
 from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 db = SQLAlchemy()
 argon2 = Argon2()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Mentecapto#171@localhost/adlances'
+    app.config.from_object(Config)
     
     argon2.init_app(app)
     db.init_app(app)
