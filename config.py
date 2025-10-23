8590661fa1e9d860c30cb24fa78a971dc75db977
KenjiOM <kenjimorimoto08@gmail.com>
2025-10-23 02:30:45 -0300

    configuração ssl

--- config.py
+++ config.py
@@ -6,4 +6,5 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ENGINE_OPTIONS = {"connect_args": {"ssl": {"ssl-disabled": False}}}
     SECRET_KEY = os.getenv('SECRET_KEY', 'aderbal')
