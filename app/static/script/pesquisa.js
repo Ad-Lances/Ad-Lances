091ad8a58810e3d80743f67c298c82d1d08a6c82
KenjiOM <kenjimorimoto08@gmail.com>
2025-11-17 04:51:36 -0300

    função de pesquisa (arrumar o front end)

---
+++ app/static/script/pesquisa.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const barra_pesquisa = document.getElementById("barra-pesquisa");
+    const leiloes = document.getElementById("leiloes");
+
+    function carregar(lista) {
+        leiloes.innerHTML = "";
+        
+        if (lista.length === 0) {
+            leiloes.innerHTML = "<h2>Nenhum leilão encontrado.</h2>";
+            return;
+        }
+        lista.forEach(leilao => {
+            const div = document.createElement("div");
+            div.textContent = `${leilao.nome} - ${leilao.descricao}`;
+            leiloes.appendChild(div)
+        });
+    }
+
+    barra_pesquisa.addEventListener("input", async (e) => {
+        e.preventDefault();
+        const pesquisa = barra_pesquisa.value.trim();
+        if (pesquisa === "") {
+            leiloes.innerHTML = "";
+            return;
+        }
+        
+        const resposta = await fetch(`/pesquisar?p=${encodeURIComponent(pesquisa)}`);
+        const resultado = await resposta.json();
+        carregar(resultado)
+        
+    })
+
+})
